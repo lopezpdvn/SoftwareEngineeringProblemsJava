db40e0fc19ec8368fd3e7d7212e3ba56d8075ef6
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-12-06 11:36:22 -0600

    Added misc use of Collection Framework

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Software engineering problems in Java
 
 by [Pedro Ivan Lopez](http://pedroivanlopez.com "Pedro Ivan Lopez's website")
+
+```bash
+$ mvn test
+```
--- software-engineering-problems/src/test/java/com/pedroivanlopez/sweng/AppTest.java
+++ software-engineering-problems/src/test/java/com/pedroivanlopez/sweng/AppTest.java
@@ -1,38 +1,112 @@
 package com.pedroivanlopez.sweng;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+  @Test
+  public void evaluatesExpression() {
+    int sum = 6;
+    assertEquals(6, sum);
+  }
+
+  @Test
+  public void testDynamicArray() {
+    ArrayList<String> darr0 = new ArrayList<String>();
+    darr0.add("one");
+    darr0.add("three");
+    darr0.add(0, "zero");
+    darr0.add(2, "two");
+    darr0.add("four");
+    assertEquals(darr0.size(), 5);
+    assertEquals(darr0.remove(1), "one");
+    assertEquals(darr0.size(), 4);
+    String[] ans = new String[] {"zero", "two", "three", "four"};
+    int i = 0;
+    for(String s : darr0) {
+      assertEquals(ans[i++], s);
+    }
+    darr0.set(1, "dos");
+    ans = new String[] {"zero", "dos", "three", "four"};
+    i = 0;
+    for(String s : darr0) {
+      assertEquals(ans[i++], s);
+    }
+  }
+
+  @Test
+  public void testLinkedList() {
+    LinkedList<String> ll = new LinkedList<String>();
+    ll.add("one");
+    ll.addFirst("zero");
+    ll.add(2, "three");
+    ll.add(2, "two");
+    ll.addLast("four");
+
+    String[] ans = new String[] {"zero", "one", "two", "three", "four"};
+    int i = 0;
+    for(String s : ll) {
+      assertEquals(s, ans[i++]);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    assertEquals(5, ll.size());
+    assertEquals(ll.getFirst(), "zero");
+    assertEquals(ll.getLast(), "four");
+  }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+  @Test
+  public void testHashMap() {
+    HashMap<String, String> map = new HashMap<String, String>();
+    map.put("zero", "cero");
+    map.put("one", "uno");
+    map.put("two", "dos");
+    assertEquals(map.size(), 3);
+    assertEquals("uno", map.get("one"));
+    assertEquals(null, map.get("ONE"));
+    map.replace("one", "UNO");
+    assertEquals(map.size(), 3);
+    assertEquals("UNO", map.get("one"));
+    assertEquals("dos", map.remove("two"));
+    assertEquals(map.size(), 2);
+    assertEquals(null, map.remove("two"));
+    assertEquals(map.size(), 2);
+    assertEquals("default", map.getOrDefault("two", "default"));
+  }
+
+  @Test
+  public void testHashMapIteration() {
+    HashSet<String> ansKeys = new HashSet<String>();
+    ansKeys.add("one");
+    ansKeys.add("zero");
+    ansKeys.add("three");
+    ansKeys.add("two");
+    HashSet<Integer> ansValues = new HashSet<Integer>();
+    ansValues.add(0);
+    ansValues.add(1);
+    ansValues.add(2);
+    ansValues.add(3);
+    HashMap<String, Integer> map = new HashMap<String, Integer>();
+    map.put("one", 1);
+    map.put("zero", 0);
+    map.put("three", 3);
+    map.put("two", 2);
+
+    for(String k : map.keySet()) {
+      assertTrue(ansKeys.contains(k));
     }
+    for(Integer v : map.values()) {
+      assertTrue(ansValues.contains(v));
+    }
+    for(Map.Entry<String, Integer> entry : map.entrySet()) {
+      assertTrue(ansKeys.contains(entry.getKey()));
+      assertTrue(ansValues.contains(entry.getValue()));
+    }
+  }
 }
